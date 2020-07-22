fc56c5d3227a619f89c7a05be2bca4b916dcc6fb
Swiftly <ivecis@live.co.kr>
2020-07-22 17:33:30 +0900

    6 AND THE CITY
    AND 3UW0N

--- src/main/java/org/xoxo/Service/ServiceImpl.java
+++ src/main/java/org/xoxo/Service/ServiceImpl.java
@@ -4,15 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.xoxo.DAO.DaoImpl;
 import org.xoxo.DTO.Person;
+import org.xoxo.IDAO.IDao;
 
 @Service
 public class ServiceImpl implements IService {
     private static final Logger logger =
             LoggerFactory.getLogger(ServiceImpl.class);
     @Autowired
-    private DaoImpl personDao;
+    private IDao personDao;
+
     @Override
     public void insertProc(Person person) {
         personDao.insertProc(person);
